a7af52f69e840c49b189fde3b6f7ad8202c5894c
dmtrung14 <trungdangminh14012004@gmail.com>
2023-01-18 20:24:09 +0700

    crime

--- BitflipGenerator/src/BitflipGenerator.jl
+++ BitflipGenerator/src/BitflipGenerator.jl
@@ -1,20 +1,24 @@
 module BitflipGenerator
-    function flip_i_bit(n,i, string)
-        return bitstring(string ⊻ 1 <<i)
-        # flip from right to left
+    function flip_i_bit(i, string)
+        return string ⊻ 1 << i
+        # so we don't need bitstring() to perform the xor bitflipping
+        # return bitstring(string ⊻ 1 <<i)
+        # # flip from right to left
     end
     function bitflip_generator(SAMPLE_SIZE, length, error_rate, string)
         #for any codeword, generate a number of bitflips(or not) with probability of p.
-        res = Int8[]
+        res = String[]
         for sample in 1:SAMPLE_SIZE
             codeword = string
             for i in 1:length
-                p =rand(1)
+                p =rand()
                 if p < error_rate
-                    codeword = flip_i_bit(n,i,codeword)
+                    codeword = flip_i_bit(i, codeword)
+                    # codeword = parse(UInt, flip_i_bit(i, codeword) ; base=2)
                 end
             end
-            push!(res, codeword)
+            push!(res, last(bitstring(codeword),length))
+            #add the last length digit of bitstring to the result
         end
         
         return res
